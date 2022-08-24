2983e398635e3fb1caade935189e7b2558dd9fdb
Ironpurush0 <ridamsaxena90.com>
2022-08-24 21:33:45 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react'
 
-function App() {
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+import {FiSettings} from 'react-icons/fi'
+
+import {TooltipComponent} from '@syncfusion/ej2-react-popups'
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello worlds</h1>
+    <div>
+      <Router>
+        <div className='flex relative dark:bg-main-dark-bg'>
+          <div className='fixed right-4 bottom-4' style={{zIndex: '1000'}}>
+            <TooltipComponent content="Settings" position='top'>
+              <button type='button' style={{background: 'blue', borderRadius: '50%'}} className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
